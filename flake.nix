8de9c82b85efa805bfb1ab6f6a82e9a087c2563f
petra <zeta-6po@outlook.com>
2026-02-24 23:29:05 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Gemini CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gemini-cli-src = {
+      url = "github:google-gemini/gemini-cli/release/v0.29.3";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gemini-cli-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "gemini-cli";
+          version = "0.29.3";
+	  src = gemini-cli-src;
+
+          npmDepsFetcherVersion = 2;
+          npmDepsHash = "sha256-u+PSO4BUshpqV5TIi4OXGEIpJ2XyV0oooCRFiepnvCY=";
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            python3
+          ];
+
+          buildInputs = with pkgs; [
+            libsecret
+          ];
+          buildPhase = ''
+            npm run bundle
+          '';
+          
+          installPhase = ''
+            mkdir -p $out/bin
+            mkdir -p $out/lib/gemini-cli
+          
+            cp -rL bundle/ $out/lib/gemini-cli/bundle
+          
+            cat > $out/bin/gemini-cli <<EOF
+            #!/bin/sh
+            exec ${pkgs.nodejs}/bin/node $out/lib/gemini-cli/bundle/gemini.js "\$@"
+            EOF
+          
+            chmod +x $out/bin/gemini-cli
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs ];
+        };
+      }
+    );
+}
