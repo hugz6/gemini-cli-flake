e61f69c494089f3329fed3275f7b43e705067da8
hugz6 <69194496+hugz6@users.noreply.github.com>
2026-04-09 09:15:57 +0000

    chore: update gemini-cli to 0.37.0

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     gemini-cli-src = {
-      url = "github:google-gemini/gemini-cli?ref=refs/tags/v0.36.0";
+      url = "github:google-gemini/gemini-cli?ref=refs/tags/v0.37.0";
       flake = false;
     };
   };
@@ -17,11 +17,11 @@
       in {
         packages.default = pkgs.buildNpmPackage {
           pname = "gemini-cli";
-          version = "0.36.0";
+          version = "0.37.0";
 	  src = gemini-cli-src;
 
           npmDepsFetcherVersion = 2;
-          npmDepsHash = "sha256-WM+BWGnwYkAvhqZ0h56PXZNMsNG3Q5TmXkpLmewLnRE=";
+          npmDepsHash = "sha256-UqyIblEqanFlCUt5RidUlp0PPCAoAa3VM+GIW6mYQh8=";
           nativeBuildInputs = with pkgs; [
             pkg-config
             python3
